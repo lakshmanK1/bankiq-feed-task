615aa95cc898dfe0f53e8086bfa68a7692c395dd
lakshmanK1 <laxmankamisetty1@gmail.com>
2023-05-02 18:18:00 +0530

    implemented pagination

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,40 +1,55 @@
-import React,{useEffect, useState} from 'react'
-import './Feed.css'
+import { useEffect, useState } from 'react';
+import './FeedCmp.css';
 
-function Feed() {
-    const [feedData, setFeedData] = useState([]);
+function FeedCmp() {
+  const [products, setProducts] = useState([])
+  const [page, setPage] = useState(1)
 
-    const fetchApiFeed = () => {
-        fetch('https://dummyjson.com/products').then((res)=>res.json()).then((data)=>{
-            setFeedData(data.products);
-        })
+  const fetchProducts = async () => {
+    const res = await fetch(`https://dummyjson.com/products?limit=100`)
+    const data = await res.json()
+
+    console.log(data);
+
+    if (data && data.products) {
+      setProducts(data.products)
     }
-    
-    useEffect(()=>{
-        fetchApiFeed();
-    },[]); 
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  const selectPageHandler = (selectedPage) => {
+    if (selectedPage >= 1 && selectedPage <= products.length / 10 && selectedPage !== page) {
+      setPage(selectedPage)
+    }
+  }
 
   return (
     <div>
-        {
-            feedData.length > 0 && 
-            <div className='feeds'>
-                {
-                    feedData.map((data)=>{
-                        return(
-                            <span className='feed__single' key={data.id}>
-                                <img src={data.thumbnail} alt='image'/>
-                                <span>Title: {data.title}</span>
-                                <span>Descriptions: {data.description}</span>
-                                <span>LastUpload: {data.dateLastEdited}</span>
-                            </span>
-                        );
-                    })
-                }
-            </div>
-        }
+      {products.length > 0 && <div className="feeds">
+        {products.slice(page * 10 - 10, page * 10).map((prod) => {
+          return <span className="feed__single" key={prod.id}>
+            <img src={prod.thumbnail} alt={prod.title} /> {/* alt is imp */}
+            <span>
+              {prod.title}
+            </span>
+          </span>
+        })}
+      </div>}
+
+      {products.length > 0 && <div className="pagination">
+        <span onClick={() => selectPageHandler(page - 1)} className={page > 1 ? "" : "pagination__disable"}>◀</span>
+
+        {[...Array(products.length / 10)].map((_, i) => {
+          return <span key={i} className={page === i + 1 ? "pagination__selected" : ""} onClick={() => selectPageHandler(i + 1)}>{i + 1}</span>
+        })}
+
+        <span onClick={() => selectPageHandler(page + 1)} className={page < products.length / 10 ? "" : "pagination__disable"}>▶</span>
+      </div>}
     </div>
-  )
+  );
 }
 
-export default Feed
+export default FeedCmp;
